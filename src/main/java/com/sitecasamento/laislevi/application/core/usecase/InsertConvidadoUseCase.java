266e97335410e2be0144e9c894c8d7704a6e664e
Levi Sanches <levi_sanches@outlook.com>
2025-03-29 08:56:58 -0300

    Ajuste de lógica

--- src/main/java/com/sitecasamento/laislevi/application/core/usecase/InsertConvidadoUseCase.java
+++ src/main/java/com/sitecasamento/laislevi/application/core/usecase/InsertConvidadoUseCase.java
@@ -21,9 +21,9 @@ public class InsertConvidadoUseCase implements InsertConvidadoInputPort {
         if (convidadoDTO != null) {
             ConvidadoEntity convidado = new ConvidadoEntity(convidadoDTO);
             for (AcompanhanteDTO acompanhanteDTO : convidadoDTO.getAcompanhantes()) {
-                if(acompanhanteDTO.getNome().isBlank() || acompanhanteDTO.getNome().isEmpty()) {
+                if(!acompanhanteDTO.getNome().isBlank() || !acompanhanteDTO.getNome().isEmpty()) {
                     AcompanhanteEntity acompanhante = new AcompanhanteEntity(acompanhanteDTO);
-                    convidado.addAcompanhante(acompanhante);
+                    convidado.addAcompgitanhante(acompanhante);
                 }
             }
             convidadoRepositoryOutputPort.save(convidado);
